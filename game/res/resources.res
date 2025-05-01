47749c6909a6a8fb663024d9ff4f4cb82b36d2f0
Victor Briganti <jvbriganti@gmail.com>
2025-05-01 17:05:34 -0300

    game: Load heart sprite

--- game/res/resources.res
+++ game/res/resources.res
@@ -1,1 +1,5 @@
-IMAGE background "backgrounds/background1.png" FAST
+IMAGE background "backgrounds/background.png" FAST
+
+SPRITE heart_full "hud/heart_full.png" 2 2 FAST
+SPRITE heart_half "hud/heart_half.png" 2 2 FAST
+SPRITE heart_empty "hud/heart_empty.png" 2 2 FAST
--- game/src/main.c
+++ game/src/main.c
@@ -8,8 +8,34 @@ void background_init() {
     VDP_setScreenWidth320();
     SPR_init();
 
-    VDP_drawImageEx(BG_B, &background, TILE_ATTR_FULL(PAL1, 0, 0, 0, 1), 0, 0,
+    VDP_drawImageEx(BG_B, &background, TILE_ATTR_FULL(PAL3, 0, 0, 0, 1), 0, 0,
                     true, DMA);
+}
+
+void text_init() {
+    // Text rendering
+    char score[6];
+    sprintf(score, "%d", 65443);
+    VDP_drawText(score, 0, 1);
+    VDP_drawText("LEVEL 1-1", 13, 1);
+}
+
+Sprite *heart_full_init() {
+    PAL_setPalette(PAL1, heart_full.palette->data, DMA);
+    return SPR_addSprite(&heart_full, 0, 0,
+                         TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, 1));
+}
+
+Sprite *heart_half_init() {
+    PAL_setPalette(PAL1, heart_half.palette->data, DMA);
+    return SPR_addSprite(&heart_half, 0, 0,
+                         TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, 1));
+}
+
+Sprite *heart_empty_init() {
+    PAL_setPalette(PAL1, heart_empty.palette->data, DMA);
+    return SPR_addSprite(&heart_empty, 0, 0,
+                         TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, 1));
 }
 
 int main(bool resetType) {
@@ -18,12 +44,18 @@ int main(bool resetType) {
         SYS_hardReset();
     }
 
-    VDP_drawText("000000", 0, 1);
-    VDP_drawText("LEVEL 1-1", 13, 1);
     background_init();
+    text_init();
+    Sprite *sprite1 = heart_full_init();
+    Sprite *sprite2 = heart_half_init();
+    Sprite *sprite3 = heart_empty_init();
     SYS_doVBlankProcess();
 
     while (TRUE) {
+        SPR_setPosition(sprite1, 300, 5);
+        SPR_setPosition(sprite2, 282, 5);
+        SPR_setPosition(sprite3, 264, 5);
+
         // update hardware sprites table
         SPR_update();
 
