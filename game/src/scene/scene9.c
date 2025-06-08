9eb54341d3d10fb00df796cd328161c1935c8283
Victor Briganti <jvbriganti@gmail.com>
2025-06-07 23:07:52 -0300

    game/scene: Added transition for the scene 9

--- game/src/scene/scene9.c
+++ game/src/scene/scene9.c
@@ -78,10 +78,21 @@ static inline void initGlobals() {
   playerInitY = PLAYER_SCENE9_Y_POS;
 }
 
+static inline void initTransition() {
+  SPR_clear();
+  BACKGROUND_initTransition(&level3_3_transition);
+  MAP_initLevel(mapLevelHeight, mapLevelWidth);
+}
+
 static inline void initBackground() {
+  // Release the background level transition
+  BACKGROUND_release();
+  
+  // Init the scene background
   BACKGROUND_init();
   TILEMAP_init(&tileset);
-  MAP_initLevel(mapLevelHeight, mapLevelWidth);
+  
+  HEART_draw();
 }
 
 static inline void initPlayer() {
@@ -199,19 +210,39 @@ static inline void restart() {
   restartEnemies();
 }
 
+static inline bool loadingScene() {
+  static u16 count = 0;
+  static bool loading = TRUE;
+
+  if (count < 4096) {
+    if (frame % 32 == 0) {
+      count++;
+      if (count == 4096) {
+        initBackground();
+        initPlayer();
+        initEnemies();
+        loading = FALSE;
+      }
+    }
+  }
+
+  return loading;
+}
+
 //===----------------------------------------------------------------------===//
 // PUBLIC
 //===----------------------------------------------------------------------===//
 
 void SCENE9_init() {
   initGlobals();
-  initBackground();
-  initPlayer();
-  initEnemies();
+  initTransition();
   SYS_doVBlankProcess();
 }
 
 SceneId SCENE9_update() {
+  if (loadingScene())
+    return SCENE_ID_LEVEL09;
+
   updateBackground();
   if (context.turn == PLAYER)
     updatePlayer();
@@ -265,9 +296,11 @@ void SCENE9_destroy() {
       context.enemies[i].destroy(&context.enemies[i]);
   }
 
-  // Destroy Player
+  // Release everything else
   PLAYER_destroy();
-  HEART_update();
+  HEART_release();
+  BACKGROUND_release();
+  SPR_defragVRAM();
 
   SYS_doVBlankProcess();
 }
